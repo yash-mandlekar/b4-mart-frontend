1397e949277310da0d729301f7cce35ad37f853a
manish-mandlekar <mmandlekar88@gmail.com>
2024-10-22 23:07:58 +0530

    up

--- src/components/user/UserOrders.jsx
+++ src/components/user/UserOrders.jsx
@@ -17,7 +17,39 @@ const UserOrders = () => {
           status: "Delivered",
           deliveryDate: "October 15, 2024",
           imageUrl: "https://imgs.search.brave.com/nwJbnPrWDL3wWe7WwkbH7grU5ilfVnQGRTaib5XStSM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NTFHYURGN1BwMUwu/anBn"
-        }
+        },
+        {
+            id: 1,
+            productName: "Wireless Bluetooth Headphones",
+            price: "$150",
+            status: "In Transit",
+            deliveryDate: "October 25, 2024",
+            imageUrl: "https://imgs.search.brave.com/Xo3fxq3e5QT57_yAp_a7AWU-ghIl-VKKMT0E-_7bPKA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NDFKQUNXVC13V0wu/anBn"
+          },
+          {
+            id: 2,
+            productName: "Smartphone Case",
+            price: "$25",
+            status: "Delivered",
+            deliveryDate: "October 15, 2024",
+            imageUrl: "https://imgs.search.brave.com/nwJbnPrWDL3wWe7WwkbH7grU5ilfVnQGRTaib5XStSM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NTFHYURGN1BwMUwu/anBn"
+          },
+          {
+            id: 1,
+            productName: "Wireless Bluetooth Headphones",
+            price: "$150",
+            status: "In Transit",
+            deliveryDate: "October 25, 2024",
+            imageUrl: "https://imgs.search.brave.com/Xo3fxq3e5QT57_yAp_a7AWU-ghIl-VKKMT0E-_7bPKA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NDFKQUNXVC13V0wu/anBn"
+          },
+          {
+            id: 2,
+            productName: "Smartphone Case",
+            price: "$25",
+            status: "Delivered",
+            deliveryDate: "October 15, 2024",
+            imageUrl: "https://imgs.search.brave.com/nwJbnPrWDL3wWe7WwkbH7grU5ilfVnQGRTaib5XStSM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NTFHYURGN1BwMUwu/anBn"
+          },
       ];
   return (
     <div className="order-page">
