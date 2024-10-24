7a72ff15e08a79c19fd67b9eef5589e89213570a
surya-rathore <sahusurajbali@gmail.com>
2024-10-24 13:59:36 +0530

    uder order is ready

--- src/components/user/UserOrders.jsx
+++ src/components/user/UserOrders.jsx
@@ -20,28 +20,33 @@ const UserOrders = () => {
             <div className="card__skeleton card__description"> </div>
             <div className="card__skeleton card__description"> </div>
           </div>
-        ) : (
+        ) :  (
           orders.map((order) => (
-            <div className="order-card" key={order._id}>
-              <img
-                src={order.imageUrl}
-                alt={order.productName}
-                className="order-image"
-              />
-              <div className="order-details">
-                <h2>{order.productName}</h2>
-                <p>Price: {order.price}</p>
-                <p>
-                  Status: <strong>{order.status}</strong>
-                </p>
-                <button className="track-button">Cancel Order</button>
-              </div>
+            <div className="order-card">
+              {order.products.map((product_detail) => (
+                <div key={product_detail.product._id} className="order-item">
+                  
+                  <img
+                    src={product_detail.product.productpic[0]}
+                    alt={product_detail.product.product_name}
+                    className="order-image"
+                  />
+                  <div className="order-details">
+                    <h2>{product_detail.product.product_name}</h2> 
+                    <p><b>Price:</b> ₹{product_detail.product.price}</p> 
+                    <p><b>Quantity:</b> {product_detail.count}</p> 
+                    <p>
+                      <b>Status:</b> <strong>{order.orderStatus}</strong>
+                    </p>
+                    <button className="track-button">Cancel Order</button>
+                  </div>
+                </div>
+              ))}
             </div>
-          ))
-        )}
-      </div>
+          )))}
+       </div>   
     </div>
-  );
-};
+    )
+}
 
 export default UserOrders;
